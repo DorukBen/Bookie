371df80c2d17feccd7cb39681921fad26547e01f
Doruk <dorukerena@gmail.com>
2017-01-04 20:18:48 +0300

    buttons added

--- app/src/main/java/com/karambit/bookie/LocationActivity.java
+++ app/src/main/java/com/karambit/bookie/LocationActivity.java
@@ -1,8 +1,14 @@
 package com.karambit.bookie;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
@@ -23,6 +29,11 @@ public class LocationActivity extends AppCompatActivity {
             getSupportActionBar().setElevation(0);
         }
 
+        final Button okButton = (Button) findViewById(R.id.locationOKButton);
+        okButton.setEnabled(false);
+        okButton.setTextColor(ContextCompat.getColor(LocationActivity.this, R.color.secondaryTextColor));
+        okButton.setAlpha(0.5f);
+
         SupportPlaceAutocompleteFragment autocompleteFragment = (SupportPlaceAutocompleteFragment)
                 getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
@@ -36,12 +47,23 @@ public class LocationActivity extends AppCompatActivity {
             @Override
             public void onPlaceSelected(Place place) {
                 //TODO: set user location here place.getLat()
+                okButton.setEnabled(true);
+                okButton.setTextColor(ContextCompat.getColor(LocationActivity.this, R.color.colorAccent));
+                okButton.setAlpha(1f);
             }
 
             @Override
             public void onError(Status status) {
-
             }
         });
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 }
