3b2db9fd104190935d71c84637e9211ebe0b6af0
dorukBen <dorukerena@gmail.com>
2017-03-11 00:18:37 +0200

    Current User Settings internet connection completed

--- app/src/main/java/com/karambit/bookie/rest_api/UserApi.java
+++ app/src/main/java/com/karambit/bookie/rest_api/UserApi.java
@@ -1,5 +1,7 @@
 package com.karambit.bookie.rest_api;
 
+import android.support.annotation.Nullable;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -28,7 +30,7 @@ public interface UserApi {
                                 @Field("nameSurname") String nameSurname);
 
     @FormUrlEncoded
-    @POST("addLovedGenre")
+    @POST("AddLovedGenre")
     Call<ResponseBody> setLovedGenres(@Field("email") String email,
                                       @Field("password") String password,
                                       @Field("genreCodes") String lovedGenres);
@@ -37,4 +39,38 @@ public interface UserApi {
     Call<ResponseBody> getUserProfilePageComponents(@Query("email") String email,
                                                     @Query("password") String password,
                                                     @Query("userID") int anotherUserId);
+
+    @FormUrlEncoded
+    @POST("UpdateUserDetails")
+    Call<ResponseBody> updateUserDetails(@Field("email") String email,
+                                      @Field("password") String password,
+                                      @Field("name") String name,
+                                      @Field("bio") String bio,
+                                      @Field("latitude") double latitude,
+                                      @Field("longitude") double longitude);
+
+    @FormUrlEncoded
+    @POST("UpdateUserDetails")
+    Call<ResponseBody> updateUserDetails(@Field("email") String email,
+                                         @Field("password") String password,
+                                         @Field("name") String name,
+                                         @Field("bio") String bio);
+
+    @FormUrlEncoded
+    @POST("Feedback")
+    Call<ResponseBody> uploadFeedBack (@Field("email") String email,
+                                         @Field("password") String password,
+                                         @Field("feedback") String name);
+
+    @FormUrlEncoded
+    @POST("UserValidation")
+    Call<ResponseBody> isPasswordCorrect (@Field("email") String email,
+                                       @Field("password") String password,
+                                       @Field("givenPassword") String givenPassword);
+
+    @FormUrlEncoded
+    @POST("UserChangePassword")
+    Call<ResponseBody> uploadNewPassword (@Field("email") String email,
+                                          @Field("password") String password,
+                                          @Field("newPassword") String newPassword);
 }
